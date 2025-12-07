6252660f8283dc177a554a89b7087361998c4044
Juan David Cuero Sarria <davidjuanmontano7@gmail.com>
2025-12-07 01:50:53 +0000

    hazlo

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
